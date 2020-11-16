07384c36323334acad0b59d95a5b3fbe80d0e30e
Kattyabo <k.barrera.ordenes@gmail.com>
2020-11-15 23:06:30 -0300

    JS

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+function acceder(){
+
+if (document.form.email.value =='k@k' && document.form.password.value =='k@k'){ 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña entregados en README."); 
+    } 
+} 
+
